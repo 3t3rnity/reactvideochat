54c08cc562d68ab996c0708e89029a69a1bd6bad
3t3rnity <q.makc@mail.ru>
2021-04-22 23:37:17 +0300

    server complete

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const http = require("http");
 const app = express();
-const server = require("http").Server(app);
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-server.listen(3030);
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(3030, () => console.log("server is runnin"));
